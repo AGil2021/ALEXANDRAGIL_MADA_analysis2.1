d9fbd8143cccad82fc204a219ebea3a2a1ad2ea7
Alexandra Gil <ag66152@uga.edu>
2021-09-15 21:43:37 -0400

    Data Analysis pt 2
    Used processeddata to create analysis of Age and Condition at time of Covid-19 death. Updated manuscript with figures.

--- code/analysis_code/analysisscript.R
+++ code/analysis_code/analysisscript.R
@@ -8,10 +8,11 @@
 library(ggplot2) #for plotting
 library(broom) #for cleaning up output from lm()
 library(here) #for data loading/saving
+library(tidyverse) 
 
 #path to data
 #note the use of the here() package and not absolute paths
-data_location <- here::here("data","processed_data","processeddata.rds")
+"~/Documents/School/Fall 2021/MADA/ALEXANDRAGIL_MADA_analysis2.1/ <- here::here("data","processed_data","processeddata.rds")
 
 #load data. 
 mydata <- readRDS(data_location)
@@ -31,39 +32,64 @@ print(mysummary)
 
 #do the same, but with a bit of trickery to get things into the 
 #shape of a data frame (for easier saving/showing in manuscript)
+
 summary_df = data.frame(do.call(cbind, lapply(mydata, summary)))
 
 #save data frame table to file for later use in manuscript
 summarytable_file = here("results", "summarytable.rds")
 saveRDS(summary_df, file = summarytable_file)
 
+#now lets take another look
+print(summary_df)
 
-#make a scatterplot of data
-#we also add a linear regression line to it
-p1 <- mydata %>% ggplot(aes(x=Height, y=Weight)) + geom_point() + geom_smooth(method='lm')
+#For the analysis, lets look at the predominant condition contributing with COVID-19 deaths according to age group
 
-#look at figure
-plot(p1)
+#From the summary I can see that COVID.19.Deaths are categorical. Let change to numeric
+
+mydata$COVID.19.Deaths <- as.numeric(mydata$COVID.19.Deaths)
+
+#Double check
+class(mydata$COVID.19.Deaths)
+
+#Since age is categorical, this will have to be presented in a bar graph or table
+#Lets transform this data from long format to wide format with age group into rows
+#When transforming from long to wide, you will inevbitably get NA. 
+#Since we are adding the sums in the table, I changed all NA to 0 to avoid an error
+
+mydata[is.na(mydata)] = 0
+
+#Now we can use pivot wider
+agetbl <- pivot_wider(mydata, names_from = c(Age.Group), values_from = COVID.19.Deaths, Condition,  values_fn = sum)
+
+print(agetbl)
+
+#save data table 
+table1_file = here("results", "table1.rds")
+saveRDS(agetbl, file = table1_file)
+
+#make a barchart
+plot1data <- mydata[c("COVID.19.Deaths","Age.Group")]
+plot1 <- ggplot(plot1data, aes(Age.Group, COVID.19.Deaths)) + geom_bar(stat = "identity")
+
+plot(plot1)
 
 #save figure
-figure_file = here("results","resultfigure.png")
-ggsave(filename = figure_file, plot=p1) 
+figure_file1 = here("results", "resultfigure1.png")
+ggsave(filename = figure_file1, plot=plot1) 
 
-######################################
-#Data fitting/statistical analysis
-######################################
 
-# fit linear model
-lmfit <- lm(Weight ~ Height, mydata)  
+#Now lets look at death in children from COVID-19
+childdata <- mydata[ which(mydata$Age.Group=="0-24"), ]
 
-# place results from fit into a data frame with the tidy function
-lmtable <- broom::tidy(lmfit)
+#Looking at condition in those ages 0-24
+plot2data <- childdata[c("COVID.19.Deaths","Condition")]
+plot2 <- ggplot(plot2data, aes(Condition, COVID.19.Deaths)) + geom_bar(stat = "identity")
+plot2.1 <- plot2 + coord_flip()
 
-#look at fit results
-print(lmtable)
+plot(plot2.1)
 
-# save fit results table  
-table_file = here("results", "resulttable.rds")
-saveRDS(lmtable, file = table_file)
 
-  
+#save figure
+figure_file2 = here("results", "resultfigure2.1.png")
+ggsave(filename = figure_file2, plot=plot2.1) 
+
--- code/processing_code/processingscript.R
+++ code/processing_code/processingscript.R
@@ -1,66 +1,51 @@
-###############################
-# processing script
+# processing scrip
 #
-#this script loads the raw data, processes and cleans it 
-#and saves it as Rds file in the processed_data folder
+# Process the raw data
 
-#load needed packages. make sure they are installed.
-library(readxl) #for loading Excel files
+library(readr) # for load a CSV file
 library(dplyr) #for data processing
 library(here) #to set paths
 
-#path to data
-#note the use of the here() package and not absolute paths
-data_location <- here::here("data","raw_data","exampledata.xlsx")
+# The data using in this assignment is from the CDC https://data.cdc.gov/NCHS/Conditions-Contributing-to-COVID-19-Deaths-by-Stat/hk9y-quqm
+# This dataset shows health conditions and contributing causes mentioned in conjunction with deaths involving coronavirus disease 2019 (COVID-19) 
+# by age group and jurisdiction of occurrence.
 
-#load data. 
-#note that for functions that come from specific packages (instead of base R)
-# I often specify both package and function like so
-#package::function() that's not required one could just call the function
-#specifying the package makes it clearer where the function "lives",
-#but it adds typing. You can do it either way.
-rawdata <- readxl::read_excel(data_location)
+# The here package creates paths relative to the top-level directory.
+# The package displays the top-level of the current project on load or any time you call
 
-#take a look at the data
-dplyr::glimpse(rawdata)
+data_spot<- here::here("data","raw_data","Conditions_Contributing_to_COVID-19_Deaths__by_State_and_Age__Provisional_2020-2021.csv")
+#Loading data
+raw_data <- read.csv(data_spot)
+# Checking the data 
+dplyr::glimpse(raw_data)
+# The data is particularly organized by groups: "By Total" or "By Year",. 
+# "By Total" we can see the cumulative data per condition by age. 
+# "By year" we can see the data from 2020 and 2021 by condition by age.
+# "By Month" we can see the date by evey single month in both years by condition  by age.
+#For this particular analysis we will use "By year" data, in order to see
+# the behavior of some particular conditions by age in year 2020 and year 2021.
+# The Flag variable counts less than 10 supressed. It takes in consideration values between 1 and 9. The we will removed those values from the data.
 
-#dataset is so small, we can print it to the screen.
-#that is often not possible.
-print(rawdata)
+raw_data[-c("Flag"=="One or more"),]
+processeddata <- filter(raw_data, Group=="By Year") %>%
+  select(-"Month",-"ICD10_codes", -"Flag")
 
-# looks like we have measurements for height (in centimeters) and weight (in kilogram)
+#looking for missing data
+is.na(processeddata)
 
-# there are some problems with the data: 
-# There is an entry which says "sixty" instead of a number. 
-# Does that mean it should be a numeric 60? It somehow doesn't make
-# sense since the weight is 60kg, which can't happen for a 60cm person (a baby)
-# Since we don't know how to fix this, we need to remove the person.
-# This "sixty" entry also turned all Height entries into characters instead of numeric.
-# We need to fix that too.
-# Then there is one person with a height of 6. 
-# that could be a typo, or someone mistakenly entered their height in feet.
-# Since we unfortunately don't know, we'll have to remove this person.
-# similarly, there is a person with weight of 7000, which is impossible,
-# and one person with missing weight.
-# to be able to analyze the data, we'll remove those 5 individuals
+# Observations with NA values will be deleted
 
-# this is one way of doing it. Note that if the data gets updated, 
-# we need to decide if the thresholds are ok (newborns could be <50)
+processeddata <- processeddata %>% na.omit(processeddata)
 
-processeddata <- rawdata %>% dplyr::filter( Height != "sixty" ) %>% 
-                             dplyr::mutate(Height = as.numeric(Height)) %>% 
-                             dplyr::filter(Height > 50 & Weight < 1000)
+# Now the processeddata could be considerated as clean and we can start the analysis with:
+# - What is the predominant condition contributing with COVID-19 deaths according with the age,
+# we can do this by state or in general in United states.
+# - We also, can make comparisons between year 2020 ans 2021 and see if the conditions are the same for both years.
+# - What state(s) have the highest death rate according with the predominant condition in USA
 
-# save data as RDS
-# I suggest you save your processed and cleaned data as RDS or RDA/Rdata files. 
-# This preserves coding like factors, characters, numeric, etc. 
-# If you save as CSV, that information would get lost.
-# See here for some suggestions on how to store your processed data:
-# http://www.sthda.com/english/wiki/saving-data-into-r-data-format-rds-and-rdata
+# Now is time to save the data as RDS
+save_data_spot<- here::here("data","processed_data","processeddata.rds")
 
-# location to save file
-save_data_location <- here::here("data","processed_data","processeddata.rds")
-
-saveRDS(processeddata, file = save_data_location)
+saveRDS(processeddata, file = save_data_spot)
 
 
